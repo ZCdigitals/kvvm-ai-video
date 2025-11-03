05fc63ce4e478c1f14a442c47defeb7b80522345
alex <liuhlightning@qq.com>
2025-11-03 16:10:19 +0800

    fix args

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 2
-#define VERSION_BUILD 5
+#define VERSION_BUILD 6
 
 #define STRINGIFY(s) #s
 #define TOSTRING(s) STRINGIFY(s)
